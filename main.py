1d0fade554f6216e1602376e1657b9bf072939f3
gustavo <britopereiragustavo@gmail.com>
2022-09-18 21:12:35 -0300

    Removing PathUtils class from main.py and isolating it in utils.py

--- main.py
+++ main.py
@@ -1,58 +1,7 @@
 import re
 from urllib.parse import urlparse, urlunparse, urlencode, parse_qsl
 
-
-class PathUtils(str):
-    def __truediv__(self, other):
-        """Soma os caminhos relativos considerando os separadores"""
-        if self.endswith('/'):
-            value = self + other.lstrip('/')
-        elif other.startswith('/'):
-            value = self + other
-        else:
-            value = self + '/' + other
-        return self.__class__(self.sanitize_path(value))
-
-    @staticmethod
-    def sanitize_path(path):
-        value = path.replace('//', '/').replace(' ', '')
-        if not value.startswith('/'):
-            value = f'/{value}'
-        value = value.split('?')[0]
-        value = value.split('#')[0]
-        return value
-
-    @staticmethod
-    def sanitize_anchor(anchor, with_sharp=False):
-        value = anchor.replace('//', '/').replace(' ', '').replace('#', '')
-        if value and with_sharp and not value.startswith('#'):
-            value = f'#{value}'
-        return value
-
-    @staticmethod
-    def sanitize_query(query):
-        if not query:
-            return ''
-        value = query.replace(' ', '')
-        if not value.startswith('?'):
-            value = f'?{value}'
-        return value
-
-    @staticmethod
-    def dismember_path(path):
-        query = path.split('?')
-        if len(query) > 1:
-            query = query[1].split('#')
-            query = query[0]
-        else:
-            query = ''
-
-        anchor = path.split('#')
-        if len(anchor) > 1:
-            anchor = anchor[1]
-        else:
-            anchor = ''
-        return query, anchor
+from utils import PathUtils
 
 
 class SmartPath:
